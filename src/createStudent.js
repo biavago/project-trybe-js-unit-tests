5ee68398de578f1c3159f9bb9036d11ede844930
beatriz <beatriz.evago@gmail.com>
2022-10-21 17:20:01 +0100

    Realizado o requisito 5.

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,15 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const student = {
+    name,
+  };
+  student.name = name;
+  student.feedback = function () {
+    return 'Eita pessoa boa!';
+  };
+  return student;  
+};
 
 module.exports = createStudent;
