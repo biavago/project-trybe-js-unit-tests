98ab95a6fdf8bd245795c1bfac4095a659a124fc
beatriz <beatriz.evago@gmail.com>
2022-10-23 20:08:00 +0100

    Realizado o requisito 6.

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -18,7 +18,6 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contendo os valores esperados', () => {
-    // fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
     expect(circle(NaN)).toBeUndefined();
--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,13 +31,18 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
+    expect(typeof productDetails).toBe('function');
     // Teste se o retorno da função é um array.
+    expect(typeof productDetails('a', 'b')).toBe('object');
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(Object.keys(productDetails()).length).toBe(2);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(typeof productDetails('a', 'b')[0]).toBe('object');
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(productDetails('a', 'b')).not.toEqual(productDetails('c', 'd'));
     // Teste se os dois productIds terminam com 123.
+    expect(productDetails('a', 'b')[0].details).toContais('123');
   });
 });
